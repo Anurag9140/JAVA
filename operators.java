53febb0ecceddc354c88975c80f5d4355fdc6de1
Anurag9140 <152633495+Anurag9140@users.noreply.github.com>
2024-10-07 17:40:07 +0530

    Add files via upload

---
+++ operators.java
@@ -0,0 +1,48 @@
+public class operators {
+    public static void main(String args[]){
+        int A=13;
+        int B=A--;
+        // System.out.println("ADDITION="+(A+B));
+        // System.out.println("SUBSTRACTION="+(A-B));
+        // System.out.println("MULTIPLY = "+(A*B));
+        // System.out.println("DIVISION="+(A/B));
+        // System.out.println("MODULO="+(A%B));
+        // System.out.println(A);
+        // System.out.print(B);
+    
+    // RELATIONAL OPERATORS
+        // int a=3;
+        // int b=3;
+        // System.out.println(a==b);   
+        
+        // int a=3;
+        // int b=6;
+        // System.out.println(a!=b);
+        
+        // int a=3;
+        // int b=9;
+        // System.out.println(a<b)
+        
+        // int a=2;
+        // int b=3;
+        // System.out.println(a<=b);      
+
+    //Logical operators
+    
+    System.out.println((3>2) &&(5>2));
+    System.out.println((3>5)||(5>2));
+    System.out.println(!(3<5));
+
+    
+        
+      int x = 2, y = 5;
+          int exp1 = (x * y / x);
+          int exp2 = (x * (y / x));
+          System.out.println(exp1);
+          System.out.print(exp2);
+        
+      
+    
+    }
+    
+}
