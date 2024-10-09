125ba639c3e55d00cdba7018048dab6bba4f3dbe
Anurag9140 <152633495+Anurag9140@users.noreply.github.com>
2024-10-09 20:23:31 +0530

    Add files via upload

---
+++ loops.java
@@ -0,0 +1,125 @@
+import java.util.*;
+public class loops {
+    // public static void main(String args[]){
+    //     int counter=0;
+    //     while(counter<10){
+    //     System.out.println("Hello World");
+    //     counter++;
+    // }
+//         int count=1;
+//         while(count<=10){
+//             System.out.println(count);
+//             count++;
+//         }
+//         System.out.print("This is your final output");
+
+
+       
+//         Scanner sc = new Scanner(System.in);
+//         System.out.print("Enter the value of n :- ");
+//         int num = sc.nextInt();
+//         int sum =0;
+//         int count = 1;
+//         while(count<=num){
+//             sum+=count;
+//             count++;
+//         }
+//         System.out.println("Sum of first "+num+" numbers is "+sum);
+//         sc.close();
+
+//         // FOR LOOP
+//         for(int i=1;i<=10;i=i+2){
+//             System.out.println("ANURAG");
+//         }
+//         for(int line=1;line<=4;line++){
+//             System.out.println("* * * *");
+//         }
+
+//         int line=1;
+//         while(line<=4){
+//             System.out.println("****");
+//             line++;
+//         }
+
+       
+
+//     }
+    
+// }
+
+
+// // To print reverse of a number
+
+// // 
+
+// public class loops{
+//     public static void main(String args[]){
+//         int i=1;
+//         do{
+//         System.out.println("Hello World");
+//         i++;
+//         }while(i<=5);
+//     }
+// }
+
+// public class loops{
+//     public static void main(String args[]){
+//         for(int i=1;i<=5;i++){
+//             if(i==3){
+//                 break;
+//             }
+//             System.out.println(i);
+//         }
+//         System.out.print("I am out");
+//     }
+// }
+
+// public class loops{
+//     public static void main(String args[]){
+//         Scanner sc=new Scanner(System.in);
+//         do{
+//             System.out.println("Enter your number:");
+//             int n=sc.nextInt();
+//             if(n%10==0){
+//                 break;
+//             }
+//             System.out.println(n);
+//         }while(true);
+//     }
+// }
+
+// public class loops{
+//     public static void main(String args[]){
+//         for(int i=1;i<=8;i++){
+//             if(i==3){
+//                 continue;
+//             }
+//             System.out.println(i);
+//         }
+//     }
+// }
+        
+// public class loops{
+//     public static void main(String args[]){
+//         Scanner sc=new Scanner(System.in);
+//         do{
+//             System.out.println("Enter your number:");
+//             int n=sc.nextInt();
+//             if(n%10==0){
+//                 continue;
+//             }
+//             System.out.println("Number was"+ n);
+
+//         }while(true);
+        
+    // }
+    
+    //     public static void main(String[] args){
+    //        for(int i=0; i<5; i++) {
+    //            System.out.println("Hello");
+    //            i+=2;
+    //         }
+    //  }
+     
+    
+ }
