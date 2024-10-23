fa2e620e10ae36f8a161d85b4d2a5a37fe65cc19
Anurag9140 <152633495+Anurag9140@users.noreply.github.com>
2024-10-23 15:39:28 +0530

    Add files via upload

---
+++ patterns.java
@@ -0,0 +1,51 @@
+public class patterns{
+    public static void main (String args[]){
+        // for(int line=1; line<=4;line++){
+        //     for(int star=1;star<=line;star++){
+        //         System.out.print("*");
+        //     }
+        //     System.out.println();
+        // }
+
+        // 2ND QUESTION
+        // int n=10;
+        // for(int line=1;line<=n;line++){
+        //     for(int star=1;star<=n-line+1;star++){
+        //         System.out.print("*");
+        //     }
+        //     System.out.println();
+        
+        // 3ND QUESTION
+        // int n=4;
+        // for(int line=1;line<=n;line++){
+        //     for(int num=1;num<=line;num++){
+        //         System.out.print(num);
+        //     }
+        //     System.out.println();
+        // }
+
+        // 4th QUESTION
+        // char ch='A';
+        // int n=4;
+        // for(int line=1;line<=n;line++){
+        //     for(int chars=1;chars<=line;chars++){
+        //         System.out.print(ch);
+        //         ch++;
+        //     }
+        //     System.out.println();
+        // }
+
+        // 5th QUESTION
+        
+        // int n=5;
+        // for(int line=1;line<=n;line++){
+        //     for(int chars=1;chars<=line;chars++){
+        //     System.out.print((char)('A'+chars-1));
+        //     }  
+        //     System.out.println();
+        // }
+        
+    }
+}
+
+
